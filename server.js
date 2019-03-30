b618d17f62d9e80c8af40a32cb16cd626c651d3a
Mihai Procopi <procopy.mihai@gmail.com>
2019-03-30 15:55:15 +0200

    add git ignore and change respons for api

--- server.js
+++ server.js
@@ -41,18 +41,24 @@ server.route({
     handler: function (request, reply) {
 		
        var reqno = request.params.reqno?request.params.reqno:0;
-	     
-	   console.log(reqno);
 	   
-       connection.query('select reqno, description, date, status from requests where 0 = "'+reqno+'" or reqno = " ' + reqno + '"',
+       connection.query('select description, date, user from requests where 0 = "'+reqno+'" or reqno = " ' + reqno + '"',
        function (error, results, fields) {
        if (error) {
-		   console.log('Data not found!');
-		   return;
+		   console.log(error);
+			return getResponse(reply, {
+				success: false,
+				message: error.sqlMessage
+			});
 	   };
 
-       reply(results);
+			return getResponse(reply, {
+				success: true,
+				data: results
+			});
     });
+	
+	return;
   }
 });
 
@@ -60,16 +66,40 @@ server.route({
     method: 'POST',
     path: '/newrequest',
     handler: function (request, reply) {
+
     const description = request.payload.description;
-    const date = request.payload.date;
-	const status = request.payload.status;
+    // const date = request.payload.date;
+	// const status = request.payload.status;
 	const user = request.payload.user;
+	const dateObj = new Date();
+	let month = dateObj.getMonth() + 1;
+	month = month < 10 ? '0'+month  :month;
+	let day = dateObj.getDate();
+	day = day < 10 ? '0'+day  :day;
+	const date = dateObj.getFullYear() + '-'+month + '-'+ day;
+	const status = 1;
 
-    connection.query('INSERT INTO requests (description,date,status,user) VALUES ("' + description + '","' + date + '","' + status + '","' + user + '")',
+    connection.query('INSERT INTO requests (description,date,status,user) VALUES ("' + description + '","' + date + '","' + status + '","'+user+'")',
     function (error, results, fields) {
-        if (error) throw error;
+		if(error) {
+			return getResponse(reply, {
+				success: false,
+				message: error.sqlMessage
+			});
+		}
 
-        reply(results);
+        return getResponse(reply, {
+			success: true
+		});
     });
+	
+	return;
 }
 });
+
+function getResponse(reply, data) {
+	return reply(data)
+	.header('Access-Control-Allow-Origin', '*')
+	.header('Access-Control-Allow-Headers', 'content-type')
+	.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+}
